7fe7d8bf7775f87a27d8e3ffff8ba5ea51d457a5
Luisanaelena Marín <luisanamarin@valdusoft.com>
2021-09-30 13:25:55 -0400

    Initial commit

---
+++ app/Models/Order.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $table = 'new_orders';
+
+    protected $fillable = [
+        'user_id',
+        'product_id',
+        'amount',
+        'status',
+        'payment_id',
+        'payment_url'
+    ];
+
+    public function user(){
+        return $this->belongsTo('App\Models\User');
+    }
+
+    public function product(){
+        return $this->belongsTo('App\Models\Product');
+    }
+}
---
+++ database/migrations/2021_09_30_171124_create_new_orders_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNewOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('new_orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('product_id');
+            $table->double('amount');
+            $table->string('status', 20)->default('CREATED')->comments('CREATED - PAYED - REJECTED');
+            $table->string('payment_id')->nullable();
+            $table->string('payment_url')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('product_id')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('new_orders');
+    }
+}
